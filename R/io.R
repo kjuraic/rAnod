6a0c3547b9a08097ef20e94e48b0c7a64cd466fe
Krunoslav Juraić <kjuraic@gmail.com>
2020-03-22 15:01:26 +0100

    fixed bug in read_fluke_csv() added function current_value() for extract current values from raw string

--- R/io.R
+++ R/io.R
@@ -1,4 +1,20 @@
 
+
+#' extract numeric value from ccurrent row (remove units)
+#' @author K. Juraic
+#' @param df_col column (array) with data value and units
+#' @return extracted numeric value
+#' @importFrom forcats %>%
+#' @examples \dontrun{current_value(dat$Sample)}
+current_value <- function(df_col){
+  tmp <- df_col %>%
+    stringr::str_extract(pattern = "\\-*\\d+\\,*\\d*") %>%
+    stringr::str_replace(pattern = ",", replacement = ".") %>%
+    as.numeric()
+  return(tmp)
+}
+
+
 #' read Fluke multimeter CSV file
 #' @author K. Juraic
 #' @param file_name name of CSV file with measured data
@@ -18,5 +34,25 @@ read_fluke_csv <- function(file_name = file.choose()){
                     header = TRUE,
                     nrows = dat.header$Total.readings,
                     dec = ',')
+  dat$Sample <- current_value(dat$Sample)
+  dat$Max <- current_value(dat$Max)
+  dat$Average <- current_value(dat$Average)
+  dat$Min <- current_value(dat$Min)
+  dat$Start.Time <- dat$Start.Time %>%
+    stringr::str_replace(pattern = ",", replacement = ".") %>%
+    lubridate::dmy_hms()
+  dat$Max.Time <- dat$Max.Time %>%
+    stringr::str_replace(pattern = ",", replacement = ".") %>%
+    lubridate::dmy_hms()
+  dat$Min.Time <- dat$Min.Time %>%
+    stringr::str_replace(pattern = ",", replacement = ".") %>%
+    lubridate::dmy_hms()
+  dat$Stop.Time <- dat$Stop.Time %>%
+    stringr::str_replace(pattern = ",", replacement = ".") %>%
+    lubridate::dmy_hms()
+  dat$Start.sec <- dat$Start.Time - dat$Start.Time[1]
+  dat$Stop.sec  <- dat$Stop.Time  - dat$Stop.Time[1]
+  dat$Min.sec  <- dat$Min.Time  - dat$Min.Time[1]
+  dat$Max.sec  <- dat$Max.Time  - dat$Max.Time[1]
   return(dat)
 }
